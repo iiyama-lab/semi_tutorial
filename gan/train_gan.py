20d7e322823d1e2eba6c3cafa2678bc8ee255aa5
Masaaki Iiyama <iiyama@iiyama-lab.org>
2022-05-30 16:06:51 +0900

    jupyter notebook対応

--- gan/train_gan.py
+++ gan/train_gan.py
@@ -1,5 +1,6 @@
-if __name__ == "__main__":
-    from tqdm import tqdm
+from tqdm.notebook import tqdm
+# jupyter notebookで動かさない場合は下の方を有効にする
+# from tqdm import tqdm
 import torch
 import torch.nn as nn
 
